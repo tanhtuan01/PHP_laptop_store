9eb576c2316ee70c4954b2add0ffef97916f6469
tat <trieuanhtuan1408@gmail.com>
2024-10-19 18:57:46 +0700

    first commit with db txt

---
+++ index.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="assets/css/base.css">
+<link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+
+    <div class="home_page">
+
+        <div class="header">
+            <div class="header-content">
+                <div class="top row">
+                    <div class="logo">
+                        <a href="">
+                            <!-- <img src="https://fecredit.com.vn/wp-content/uploads/2018/12/thegioididong.png" alt=""> -->
+                        </a>
+                    </div>
+                    <div class="search">
+                        <div class="box-search">
+                            <form action="">
+                               <div class="input">
+                                    <input type="text" placeholder="Bạn tìm gì..." name="" id="ips">
+                                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                               </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                    <div class="login">
+                        <a><i class="fa-solid fa-user"></i>
+                        Đăng nhập</a>
+                    </div>
+                    <div class="cart">
+                        <a>
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            Giỏ hàng
+                        </a>
+                    </div>
+                    <div class="address">
+                        <a href="">
+                            <i class="fa-solid fa-location-dot"></i>
+                            Hà Nội
+                        </a>
+                        <i class="fa-solid fa-chevron-right sub-icon"></i>
+                    </div>
+                </div>
+                <div class="sub-row">
+                    <div class="new"></div>
+                    <div class="old"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="row">
+                <h3>Trang Chủ</h3>
+            </div>
+
+
+            <div class="row">
+            <h3>Danh Mục Laptop</h3>
+                
+            </div>
+                <div class="row">
+                    <div class="category">
+                        <a href="" class="item">
+                            <img src="https://cdnv2.tgdd.vn/mwg-static/common/Category/44/10/4410b95393b8e2be4065f181932cf3b9.png" alt="">
+                        </a>
+                        <a href="" class="item">
+                            <img src="https://cdnv2.tgdd.vn/mwg-static/common/Category/7b/25/7b256aa49ccc53d2fafc71aeff1da981.png" alt="">
+                        </a>
+                        <a href="" class="item">
+                            <img src="https://cdnv2.tgdd.vn/mwg-static/common/Category/5e/8e/5e8e0225b7f45864fb8c4dbf7b151533.png" alt="">
+                        </a>
+                        <a href="" class="item">
+                            <img src="https://cdnv2.tgdd.vn/mwg-static/common/Category/16/20/1620a7d46f9bd765e33d9e291567e90a.png" alt="">
+                        </a>
+                        <a href="" class="item">
+                            <img src="https://cdnv2.tgdd.vn/mwg-static/common/Category/93/b6/93b61bcd1237eb7871ba30a003e2352e.png" alt="">
+                        </a>
+                        <a href="" class="item">
+                            <img src="https://cdnv2.tgdd.vn/mwg-static/common/Category/64/7a/647a7e93e952189ac77d9b26a9c0637e.png" alt="">
+                        </a>
+                    </div>
+                </div>
+
+            <div class="filter">
+                
+                <div class="row">
+                    <!-- <div class="ifilter">
+                        <a class="item">
+                            <i class="fa-solid fa-filter"></i>
+                            &nbsp;
+                            Lọc
+                        </a>
+                    </div> -->
+                    
+                </div>
+
+               
+
+                <div class="row sort">
+                    Sắp xếp theo: <ul>
+                        <li>
+                            <a href="">Nổi bật</a>
+                        </li>
+                        <li>
+                            <a href="">Bán chạy</a>
+                        </li>
+                         <li>
+                            <a href="">Giảm giá</a>
+                        </li>
+                        <li>
+                            <a href="">Mới</a>
+                        </li>
+                         <li>
+                            <a href="">Giá</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+
+            <div class="products row">
+                <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button title="Xem sản phẩm" class="view"><i class="fa-regular fa-eye"></i></i>&nbsp;</button>
+                            <button title="Thêm vào giỏ hàng" class="addtocart"><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;</button>
+                        </div>
+                    </div>
+                </a>
+
+                 <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </a>
+
+                 <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </a>
+
+                 <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </a>
+
+                 <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </a>
+
+                 <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </a>
+
+                 <a class="product" href="#">
+                    <div class="box">
+                        <div class="image">
+                            <img src="https://cdn.tgdd.vn/Products/Images/44/311178/asus-vivobook-go-15-e1504fa-r5-nj776w-thumb-600x600.jpg" alt="">
+                        </div>
+                        <div class="gift">
+                            <span>
+                                Tặng Office
+                            </span>
+                        </div>
+                        <h3 class="name">
+                            Product name lorem100
+                        </h3>
+                        <div class="hardware">
+                            <span class="tag">RAM16</span>
+                            <span class="tag">SS256</span> 
+                        </div>
+                        <strong class="price">
+                            12.590.000₫
+                        </strong>
+                        <div class="box-p">
+                            <p class="price-old">14.490.000₫</p>
+                            <div class="percent">9%</div>
+                        </div>
+                        <p class="item-gift">
+                            Quà <b>1.090.000₫</b>
+                        </p>
+                        <div class="add-to-cart">
+                            <button><i class="fa-solid fa-cart-arrow-down"></i>&nbsp;Thêm vào giỏ hàng</button>
+                        </div>
+                    </div>
+                </a>
+             
+      
+       
+
+    </div>
+
+</body>
+</html>
